221d2ca3d4d3e67e27c131b4509be3713b2a030f
Oleg Vaskevich <o.vaskevich@syberry.com>
2023-05-17 10:40:17 +0300

    feat: add server prod

--- server/src/modules/prisma/prisma.service.js
+++ server/src/modules/prisma/prisma.service.js
@@ -1,3 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 
-export default new PrismaClient();
+export default new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_INNER_URL
+        ? process.env.DATABASE_INNER_URL
+        : process.env.DATABASE_URL,
+    },
+  },
+});
